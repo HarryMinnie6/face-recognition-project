14eac75a8834de2f51aa8ae078d45537d22386dc
HarryMinnie6 <56819025+HarryMinnie6@users.noreply.github.com>
2020-08-31 12:25:51 +0200

    Updates

---
+++ precache-manifest.46adbff26bf7cfb119e3e3b759a394a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08cc61f22d0c296bae798d74f9714f31",
+    "url": "/face-recognition-project/index.html"
+  },
+  {
+    "revision": "e05aea0d562ecbf12331",
+    "url": "/face-recognition-project/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "eebf63cf87bea7f5ba34",
+    "url": "/face-recognition-project/static/css/main.7e1e68db.chunk.css"
+  },
+  {
+    "revision": "e05aea0d562ecbf12331",
+    "url": "/face-recognition-project/static/js/2.e4134ff1.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/face-recognition-project/static/js/2.e4134ff1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebf63cf87bea7f5ba34",
+    "url": "/face-recognition-project/static/js/main.c2a7651e.chunk.js"
+  },
+  {
+    "revision": "b655d09affbb878c2dd0",
+    "url": "/face-recognition-project/static/js/runtime-main.08273b03.js"
+  },
+  {
+    "revision": "b197ccde8074ae2d1da28a4806eb64a4",
+    "url": "/face-recognition-project/static/media/brain1.b197ccde.jpg"
+  }
+]);
